fc2d87f1f284bee03b8e7368335c473367861928
sofiamigliorini <sofia.migliorini97@gmail.com>
2024-04-24 10:00:47 +0200

    Update and rename 0_dataset_real_creation.py to dataset_creation.py

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -13,27 +13,27 @@ from pathlib import Path
 
 if __name__ == "__main__":
     print('\n\n########## Step 0: running query_github script ##########\n')
-    subprocess.run(["python3", "-m", "src.dataset_creation.00githubViewExtraction"],
+    subprocess.run(["python3", "-m", "src.dataset_creation.00_github_view_extraction"],
                    cwd=Path(__file__).parent.parent)
 
     print('\n\n########## Step 1: running filter_multi_dev_dataset script ##########\n')
-    subprocess.run(["python3", "-m", "src.dataset_creation.01cleaning"],
+    subprocess.run(["python3", "-m", "src.dataset_creation.01_cleaning"],
                    cwd=Path(__file__).parent.parent)
 
     print('\n\n########## Step 2: running filter_lifespan_dataset script ##########\n')
-    subprocess.run(["python3", "-m", "src.dataset_creation.02filter"],
+    subprocess.run(["python3", "-m", "src.dataset_creation.02_filter"],
                    cwd=Path(__file__).parent.parent)
 
     print('\n\n########## Step 3: running detect_dc_dataset script ##########\n')
-    subprocess.run(["python3", "-m", "src.dataset_creation.03imageCommitsExtraction"],
+    subprocess.run(["python3", "-m", "src.dataset_creation.03_image_commits_extraction"],
                    cwd=Path(__file__).parent.parent)
     
     print('\n\n########## Step 4: running detect_dc_dataset script ##########\n')
-    subprocess.run(["python3", "-m", "src.dataset_creation.04imageCommitsAnalysis"],
+    subprocess.run(["python3", "-m", "src.dataset_creation.04_image_commits_analysis"],
                    cwd=Path(__file__).parent.parent)
     
     print('\n\n########## Step 5: running detect_dc_dataset script ##########\n')
-    subprocess.run(["python3", "-m", "src.dataset_creation.05sampling"],
+    subprocess.run(["python3", "-m", "src.dataset_creation.05_sampling"],
                    cwd=Path(__file__).parent.parent)
 
    
