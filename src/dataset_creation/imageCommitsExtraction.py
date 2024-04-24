d4a1f33c04e1b294e2fdbe1185aebf360bd219cf
sofiamigliorini <sofia.migliorini97@gmail.com>
2024-04-24 09:40:09 +0200

    Update imageCommitsExtraction.py

--- src/dataset_creation/imageCommitsExtraction.py
+++ src/dataset_creation/imageCommitsExtraction.py
@@ -11,8 +11,8 @@ This script allows to retrieve the commit history for images hosted on GitHub.
 from github import Github
 import csv
 import time
+from src.config import ACCESS_TOKEN
 
-ACCESS_TOKEN = 'YOUR ACCESS TOKEN'
 g = Github(ACCESS_TOKEN)
 
 
@@ -48,40 +48,40 @@ def get_image_commit_history(repo_name, image_repo_name, url, image_path, last_c
     except Exception as e:
         print(f"An error occurred: {e}")
 
+if __name__ == "__main__":
+    input_file = 'images_filtered.csv'
+    output_file = 'commit_history.csv'
+    data_list = []
 
-input_file = 'images_filtered.csv'
-output_file = 'commit_history.csv'
-data_list = []
+    with open(input_file, 'r', newline='') as infile:
+        csv_reader = csv.reader(infile)
+        next(csv_reader, None)
 
-with open(input_file, 'r', newline='') as infile:
-    csv_reader = csv.reader(infile)
-    next(csv_reader, None)
+        for row in csv_reader:
+            repo_name = row[0].strip()
+            url = row[1].strip()
+            last_commit_sha = row[7]
+            data_list.append((repo_name, url, last_commit_sha))
 
-    for row in csv_reader:
-        repo_name = row[0].strip()
-        url = row[1].strip()
-        last_commit_sha = row[7]
-        data_list.append((repo_name, url, last_commit_sha))
+    with open(output_file, 'a', newline='') as outfile:
+        csv_writer = csv.writer(outfile)
+        csv_writer.writerow(["Repository Name", "Image URL", "Repo Creation Date", "Commit SHA", "Author Name", "Author Email", "Date", "Message", "Repo Commits Until This Date"])
 
-with open(output_file, 'a', newline='') as outfile:
-    csv_writer = csv.writer(outfile)
-    csv_writer.writerow(["Repository Name", "Image URL", "Repo Creation Date", "Commit SHA", "Author Name", "Author Email", "Date", "Message", "Repo Commits Until This Date"])
+        for repo_name, url, last_commit_sha in data_list:
+            time.sleep(4)
+            print(f"Repository: {repo_name}")
+            print(f"URL: {url}")
 
-    for repo_name, url, last_commit_sha in data_list:
-        time.sleep(4)
-        print(f"Repository: {repo_name}")
-        print(f"URL: {url}")
+            url_parts = url.split("/")
 
-        url_parts = url.split("/")
+            if url_parts[2] == "github.com":
+                image_repo_owner = url_parts[3]
+                image_repo_name = url_parts[4]
+                url_parts = url_parts[7:]
 
-        if url_parts[2] == "github.com":
-            image_repo_owner = url_parts[3]
-            image_repo_name = url_parts[4]
-            url_parts = url_parts[7:]
+                image_path = "/".join(url_parts)
+                image_path = image_path.replace('%20', ' ')
+                print(image_path)
+                get_image_commit_history(repo_name, f"{image_repo_owner}/{image_repo_name}", url, image_path, last_commit_sha)
 
-            image_path = "/".join(url_parts)
-            image_path = image_path.replace('%20', ' ')
-            print(image_path)
-            get_image_commit_history(repo_name, f"{image_repo_owner}/{image_repo_name}", url, image_path, last_commit_sha)
-
-    print("-" * 40)
+        print("-" * 40)
