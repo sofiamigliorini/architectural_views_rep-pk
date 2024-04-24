b49df69e10a5ff7141edec9a9165d114ac8bf838
sofiamigliorini <sofia.migliorini97@gmail.com>
2024-04-24 09:48:28 +0200

    Add files via upload

---
+++ src/dataset_creation/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Anonymous
+Email: Anonymous
+
+Global variables relatives to parameters.
+"""
+
+
+
+""" GitHub token """
+ACCESS_TOKEN: str = ''  # FIXME insert GitHub TOKEN
+    
+""" Download folder """
+OUTPUT_FOLDER: str = '../' #FIXME insert download folder
+    
+""" Minimum number of commits """
+COMMITS_THRESHOLD: int = 10
+    
+""" Minimum number of stars """
+STARS_THRESHOLD: int = 2
+    
+""" Sample size """
+SAMPLE_SIZE: int = 373
+
